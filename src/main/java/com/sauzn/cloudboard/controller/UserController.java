5abde3b79e15a1c75770dd32d72c659b2637aff9
jinxing.a.liu <jinxing.a.liu@accenture.com>
2020-11-04 18:43:11 +0800

    验证radis是否可用，可用

--- src/main/java/com/sauzn/cloudboard/controller/UserController.java
+++ src/main/java/com/sauzn/cloudboard/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.sauzn.cloudboard.controller;
 
 import com.sauzn.cloudboard.service.UserService;
+import com.sauzn.cloudboard.util.RedisUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api(value = "desc of class")
 @RequestMapping("/testBoot")
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisUtil redisUtil;
+
     @GetMapping("getUser/{id}")
     @ApiOperation(value = "desc of method", notes = "")
 //    @CacheEvict(value="thisredis", key="'users_'+#id",condition="#id!=1")
     public String GetUser(@PathVariable int id){
+//        redisUtil.lSet("abc","abc");
+        boolean a = redisUtil.set("abc","abc2");
+        log.info("abc2:" + a);
+        String abc = String.valueOf(redisUtil.get("abc"));
+
+        log.info("abc:" + abc);
         return userService.Sel(id).toString();
     }
 }
